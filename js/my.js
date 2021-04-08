323b640422c626664d08bfec4471963e4d177f53
unknown <76191429+khai-cotrol@users.noreply.github.com>
2021-04-08 11:45:36 +0700

    swith'

---
+++ js/my.js
@@ -0,0 +1,31 @@
+let broswer = prompt('let go');
+switch (broswer) {
+    case 'edge':
+        alert("you've got the adge!");
+        break;
+    case 'Chrome':
+    case 'Firefox':
+    case 'Safari':
+    case 'Opera':
+        alert("Okay we suuport these browser too");
+        break;
+    default:
+        alert("We hope that this page look ok!");
+}
+
+
+
+let a = prompt('a?', '');
+switch (a) {
+    case '0':
+        alert(0);
+        break;
+    case '1':
+        alert(1);
+        break;
+    case '2':
+    case '3':
+        m
+        alert(2);
+        break;
+}
